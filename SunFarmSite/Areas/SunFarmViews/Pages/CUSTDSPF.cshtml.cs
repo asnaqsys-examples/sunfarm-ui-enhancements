4fd99847342f6f9185f3a6b14b1de790de8fc71a
Jorge Contreras <jcontreras@asna.com>
2023-09-27 15:29:11 -0500

    Revert "Complete example in Encore RPG (#24)"
    This reverts commit fa48bfcd6afb6b89700c090ff5ba04edcc6d3173.

--- SunFarmSite/Areas/SunFarmViews/Pages/CUSTDSPF.cshtml.cs
+++ SunFarmSite/Areas/SunFarmViews/Pages/CUSTDSPF.cshtml.cs
@@ -17,8 +17,6 @@ namespace ACME.SunFarm.SunFarmViews
     ]
     public class CUSTDSPF : DisplayPageModel
     {
-        public const int CustomerSufilePageSize = 14 + 5;
-
         public SFLC_Model SFLC { get; set; }
         public CUSTREC_Model CUSTREC { get; set; }
         public SALESREC_Model SALESREC { get; set; }
@@ -39,7 +37,7 @@ namespace ACME.SunFarm.SunFarmViews
                 FunctionKeys = "F9 09;PageUp 51:!76;PageDown 50:!77",
                 DisplayFields = "!90",
                 DisplayRecords = "!90",
-                Size = CustomerSufilePageSize,
+                Size = 14,
                 IsExpandable = false,
                 EraseFormats = "CUSTREC , SALESREC"
             )
